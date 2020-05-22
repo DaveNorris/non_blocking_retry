b0f2079200ab58a14c9ad2f605f116437ad1c0c6
DaveNorris <david.norris2@bbc.co.uk>
2020-05-22 09:18:29 +0100

    wip

--- src/main/scala/Runner.scala
+++ src/main/scala/Runner.scala
@@ -8,13 +8,13 @@ import scala.io.StdIn._
 
 object MyApp extends App {
 
+  def failedFunction = Future.failed(new Exception("An error"))
+
   println(s"Ready?")
   readLine
 
-  def failedFunction = Future.failed(new Exception("An error"))
-
   (1 to 3500) foreach { _ =>
-    Retryable(60, 500.milliseconds) {
+    OldRetryable.withRetries(60, 500.milliseconds) {
       failedFunction
     }
   }
